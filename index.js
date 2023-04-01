0d2850c68bca57315729d1f1b2fc95ff8f720956
israel diaz <israel.diaz1701@gmail.com>
2023-04-01 12:04:43 -0700

    commit tree 50232c49010bedd58d23ceba2d2e6b81a0f05d51

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Your code here
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+  
+  function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left < 180) { // assuming 400px width for the container
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
